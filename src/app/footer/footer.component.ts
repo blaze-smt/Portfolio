43d62522585e5b2828deb1130bcd8c419d4db81a
Blaze <bsmith1@students.ntc.edu>
2022-12-11 13:33:36 -0600

    Finished This Portfolio page

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  scrollToElement(pageElement: HTMLElement) {
+    var positionX = 0,
+      positionY = 0;
+
+    while (pageElement != null) {
+      positionX += pageElement.offsetLeft;
+      positionY += pageElement.offsetTop;
+      pageElement = pageElement.offsetParent as HTMLElement;
+      window.scrollTo(positionX, positionY);
+    }
   }
 
+  scrollPosition(id: string) {
+    const element = this.document.getElementById(id);
+    this.scrollToElement(element as HTMLElement);
+  }
 }
