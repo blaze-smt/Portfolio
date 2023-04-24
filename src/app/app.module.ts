c9e3b1da3c2ed0eb426912eb30d2d7e1f952d7fa
Blaze <blazewaynesmith@gmail.com>
2023-04-24 10:21:36 -0500

    Rework to tailor external visits better

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LandingComponent } from './landing/landing.component';
 import { WorksComponent } from './works/works.component';
 import { BioComponent } from './bio/bio.component';
 import { ContactComponent } from './contact/contact.component';
@@ -25,7 +24,6 @@ import { RockClimbingComponent } from './rock-climbing/rock-climbing.component';
   declarations: [
     AppComponent,
     NavbarComponent,
-    LandingComponent,
     WorksComponent,
     BioComponent,
     ContactComponent,
