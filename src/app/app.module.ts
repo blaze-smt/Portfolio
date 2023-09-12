afd28b302414ede5fd4cdd152ad58ba8274760b9
Blaze <blazewaynesmith@gmail.com>
2023-09-11 19:28:14 -0500

    new stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { SocialsComponent } from './socials/socials.component';
 import { HeroComponent } from './hero/hero.component';
+import { BooksComponent } from './books/books.component';
+import { CreativesComponent } from './creatives/creatives.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +25,8 @@ import { HeroComponent } from './hero/hero.component';
     HomeComponent,
     SocialsComponent,
     HeroComponent,
+    BooksComponent,
+    CreativesComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
