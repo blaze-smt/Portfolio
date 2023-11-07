905236564b588ff8b2c4cc7148bb9aa61b902540
Blaze <blazewaynesmith@gmail.com>
2023-11-07 15:31:58 -0600

    November Changes

--- src/app/works/works.component.ts
+++ src/app/works/works.component.ts
@@ -23,51 +23,51 @@ export class WorksComponent implements OnInit {
   filteredWorks: Works[] = this.works;
 
   // Grabs filterbtn elements
-  @ViewChildren('filterBtnWorks') filterBtns: QueryList<ElementRef>;
+  // @ViewChildren('filterBtnWorks') filterBtns: QueryList<ElementRef>;
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  filterWorks(event: Event): void {
-    // Checks if clicked element doesn't include 'active' class
-    if (!(event.target as HTMLElement).classList.value.includes('active')) {
-      // Loops through filter-btn elements
-      for (let i = 0; i < this.filterBtns.toArray().length; i++) {
-        // Checks if class has 'active' class on it
-        if (
-          this.filterBtns
-            .toArray()
-            [i].nativeElement.className.includes('active')
-        ) {
-          // If 'active' class is on. Replaces it with an empty string
-          this.filterBtns.toArray()[i].nativeElement.className = this.filterBtns
-            .toArray()
-            [i].nativeElement.className.replace('active', '');
-        }
-      }
-      (event.target as HTMLElement).classList.add('active');
+  // filterWorks(event: Event): void {
+  //   // Checks if clicked element doesn't include 'active' class
+  //   if (!(event.target as HTMLElement).classList.value.includes('active')) {
+  //     // Loops through filter-btn elements
+  //     for (let i = 0; i < this.filterBtns.toArray().length; i++) {
+  //       // Checks if class has 'active' class on it
+  //       if (
+  //         this.filterBtns
+  //           .toArray()
+  //           [i].nativeElement.className.includes('active')
+  //       ) {
+  //         // If 'active' class is on. Replaces it with an empty string
+  //         this.filterBtns.toArray()[i].nativeElement.className = this.filterBtns
+  //           .toArray()
+  //           [i].nativeElement.className.replace('active', '');
+  //       }
+  //     }
+  //     (event.target as HTMLElement).classList.add('active');
 
-      // Check if clicked element includes 'active' class
-    } else if (
-      (event.target as HTMLElement).classList.value.includes('active')
-    ) {
-      // Removes 'active' class
-      (event.target as HTMLElement).classList.remove('active');
-    }
+  //     // Check if clicked element includes 'active' class
+  //   } else if (
+  //     (event.target as HTMLElement).classList.value.includes('active')
+  //   ) {
+  //     // Removes 'active' class
+  //     (event.target as HTMLElement).classList.remove('active');
+  //   }
 
-    // Filters works array based on filter id
-    if (
-      // If filter-all or the clicked element doesnt include active reset the filteredWorks
-      (event.target as HTMLElement).id == 'filter-all' ||
-      !(event.target as HTMLElement).classList.value.includes('active')
-    ) {
-      this.filteredWorks = this.works;
-    } else {
-      // Filter works if target id matches type in this.works
-      this.filteredWorks = this.works.filter(function (value, index, arr) {
-        return value.type.includes((event.target as HTMLElement).id);
-      });
-    }
-  }
+  //   // Filters works array based on filter id
+  //   if (
+  //     // If filter-all or the clicked element doesnt include active reset the filteredWorks
+  //     (event.target as HTMLElement).id == 'filter-all' ||
+  //     !(event.target as HTMLElement).classList.value.includes('active')
+  //   ) {
+  //     this.filteredWorks = this.works;
+  //   } else {
+  //     // Filter works if target id matches type in this.works
+  //     this.filteredWorks = this.works.filter(function (value, index, arr) {
+  //       return value.type.includes((event.target as HTMLElement).id);
+  //     });
+  //   }
+  // }
 }
